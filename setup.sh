82badf4bb46c303564ef0e261de0098ff11dafe8
fuqcool <fuqcool@gmail.com>
2013-06-16 14:48:53 +0800

    link preference settings

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env
 
+function link_file() {
+    if [ -f "$2" ] || [ -L "$2" ]; then
+        rm -f "$2"
+    fi
+
+    ln -s "$1" "$2"
+}
+
 cd $(dirname $0)
 dir=$(pwd)
 
 user_home=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 
 keymap_file=$user_home/Default\ \(OSX\).sublime-keymap
+pref_file=$user_home/Preferences.sublime-settings
 
-if [ -f "$keymap_file" ] || [ -L "$keymap_file" ]; then
-    rm -f "$keymap_file"
-fi
-
-ln -s $dir/keymap.sublime-keymap "$keymap_file"
+link_file $dir/keymap.sublime-keymap "$keymap_file"
+link_file $dir/preferences.sublime-settings "$pref_file"
